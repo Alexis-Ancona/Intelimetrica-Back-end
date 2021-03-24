9f093540daafeae0afa60ccbfd395a5fef9dad11
Alexis <alexis_ancona@hotmail.com>
2021-03-23 18:40:47 -0700

    database exchanged, from SQLite3 to PostgreSQL, added credentials of the DB to settings

--- intelimetrica/settings/production.py
+++ intelimetrica/settings/production.py
@@ -10,7 +10,11 @@ ALLOWED_HOSTS = ['alexis-intelimetrica.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd2oq6faqtcoh0',
+        'HOST': 'ec2-54-211-176-156.compute-1.amazonaws.com',
+        'USER': 'uozjiqzusawzyc',
+        'PORT': 5432,
+        'PASSWORD': '5aedc2db6094cbdbe926d1ef034310229379678ec45f9c69ae26add62e338768',
     }
 }
